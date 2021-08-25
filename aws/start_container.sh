d14e5eac00bae28b64046dfcc8db73fe6130e3a0
Bobalu Smallberries <bobalu@panterasbox.com>
2021-08-24 22:50:00 -0700

    fixed container name

--- aws/start_container.sh
+++ aws/start_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 2090:8000 --name flagwaver registry.gitlab.com/panterasbox/pbbanner/panterasbox:%TAG%
+docker run -d -p 2090:8000 --name pbbanner registry.gitlab.com/panterasbox/pbbanner/panterasbox:%TAG%
